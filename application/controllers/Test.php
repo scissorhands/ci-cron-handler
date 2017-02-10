daac6e5885cdc76dfaa81af52b31f197c4e121b0
Edward Scissorhands <edward_beats@hotmail.com>
2017-02-10 17:07:43 -0600

    start project

---
+++ application/controllers/Test.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function index()
+	{
+		echo "Test";
+	}
+
+}
+
+/* End of file Test.php */
+/* Location: ./application/controllers/Test.php */
---
+++ application/models/Test_model.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test_model extends CI_Model {
+
+	
+
+}
+
+/* End of file Test_model.php */
+/* Location: ./application/models/Test_model.php */
