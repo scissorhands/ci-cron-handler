e1faf9e48d8eca29312502bc3aad4e660c50f800
Edward Scissorhands <edward_beats@hotmail.com>
2017-02-10 17:20:41 -0600

    prepare for migrations

---
+++ application/controllers/Migrate.php
@@ -0,0 +1,14 @@
+<?php
+use ciutil\controllers\Migrate as Migrations;
+
+class Migrate extends Migrations
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+
+/* End of file Migrate.php */
+/* Location: ./application/controllers/Migrate.php */
